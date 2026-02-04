0e521c12d64a1f137d45f99acd2529119822723c
sidje96 <sidneydamsma@gmail.com>
2026-02-04 11:43:17 +0100

    footer met styling toegevoegd

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 
 </script>
@@ -18,7 +19,7 @@ import Header from './components/Header.vue';
     <router-view/>
   </main>
   <footer>
-    
+    <Footer/>
   </footer>
 </template>
 
