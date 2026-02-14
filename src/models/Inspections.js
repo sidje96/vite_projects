c5efe49eec4d4d95653ea23a2f71436faa2b9587
sidje96 <sidneydamsma@gmail.com>
2026-02-14 17:53:42 +0100

    Database structuur conform case gemaakt

--- src/models/Inspections.js
+++ src/models/Inspections.js
@@ -1,10 +1,45 @@
 export function createInspection(data) {
   return {
     id: data.id,
-    inspectionDate: data.inspectionDate,
-    propertyId: data.propertyId,
-    inspectorName: data.inspectorName,
-    status: data.status,
-    summary: data.summary
+    Location: data.Location,
+    Date: data.Date,
+
+    Damage: data.Damage?.map(d => ({
+      Location: d.Location,
+      DamageNew: d.DamageNew,
+      Type: d.Type,
+      Date: d.Date,
+      Acute: d.Acute,
+      Description: d.Description,
+      Pictures: d.Pictures ?? []
+    })) ?? [],
+
+    OverdueMaintenance: data.OverdueMaintenance?.map(a => ({
+      Location: a.Location,
+      Type: a.Type,
+      Acute: a.Acute,
+      CostIndication: a.CostIndication,
+      Pictures: a.Pictures ?? []
+    })) ?? [],
+
+    TechnicalInstallations: data.TechnicalInstallations?.map(t => ({
+      Location: t.Location,
+      Type: t.Type,
+      Errors: t.Errors,
+      TestProcedure: t.TestProcedure ?? [],
+      Approved: t.Approved,
+      Remarks: t.Remarks,
+      Pictures: t.Pictures ?? []
+    })) ?? [],
+
+    Modifications: data.Modifications?.map(m => ({
+      Documentation: m.Documentation ?? [],
+      Location: m.Location,
+      ExecutedBy: m.ExecutedBy,
+      Description: m.Description,
+      Action: m.Action,
+      Remarks: m.Remarks,
+      Pictures: m.Pictures ?? []
+    })) ?? []
   }
 }
