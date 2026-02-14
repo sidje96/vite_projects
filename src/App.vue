b8c55afa3a8e7f9d107c124ecef9181e2b130599
sidje96 <sidneydamsma@gmail.com>
2026-02-14 17:42:29 +0100

    kleine style verandering

--- src/App.vue
+++ src/App.vue
@@ -26,3 +26,11 @@ const icons = [
     <Footer/>
   </v-app>
 </template>
+
+<style>
+.v-btn--variant-elevated {
+  --v-shadow-key-umbra-opacity: hsla(177, 100%, 34%, 0.4);
+  --v-shadow-key-penumbra-opacity: hsla(177, 100%, 34%, 0.3);
+  --v-shadow-key-ambient-opacity: hsla(177, 100%, 34%, 0.2);
+}
+</style>
