e44c08ca17323fe50abbad5901734db0ae1627de
sidje96 <sidneydamsma@gmail.com>
2026-02-27 20:13:39 +0100

    poging 3

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,5 @@ input:-webkit-autofill:hover,
 input:-webkit-autofill:focus {
   -webkit-box-shadow: 0 0 0px 1000px hsl(0, 0%, 12%) inset !important;
   -webkit-text-fill-color: white !important;
-  caret-color: white !important;
 }
 </style>
