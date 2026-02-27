db96c8f63f17d17627513a0964b09d11c4dc4847
sidje96 <sidneydamsma@gmail.com>
2026-02-27 19:23:13 +0100

    gekke autofill bug fix

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,12 @@
   --v-shadow-key-penumbra-opacity: hsla(177, 100%, 34%, 0.3);
   --v-shadow-key-ambient-opacity: hsla(177, 100%, 34%, 0.2);
 }
+
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus {
+  -webkit-box-shadow: 0 0 0px 1000px hsl(0, 0%, 12%) inset !important;
+  -webkit-text-fill-color: white !important;
+}
+
 </style>
