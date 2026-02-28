f07130c0bf250f40b260dda5e804e88ffab299fc
sidje96 <sidneydamsma@gmail.com>
2026-02-28 16:33:00 +0100

    icon plus text verandert voor afbeelding van hetzelfde

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-toolbar :elevation="10" color="black">
+    <v-toolbar :elevation="10" color="hsl(0, 0%, 12%)">
         <router-link to="/">
-            <Icon color="white" size="64" class="d-flex align-center" />
+            <v-img 
+                src="/src/assets/LogoWithText.png"
+                :width=" xs ? 150 : 250"
+                contain
+            />
         </router-link>
-        <div class="d-flex flex-column align-xs-center">       
-            <span class="text-subtitle-1 text-sm-h6 text-md-h5">Real Estate Care</span>
-            <span class="text-caption text-sm-subtitle-2 text-md-subtitle-1">Caring is in our nature</span>
-        </div>
         <v-spacer />        
         <v-hover v-slot="{ isHovering, props }">
             <v-btn
@@ -33,7 +33,6 @@
 
 <script setup>
 import { useRouter } from 'vue-router'
-import Icon from './icons/icon.vue'
 import { useDisplay } from 'vuetify'
 import { useAuthStore } from '@/stores/auth'
 
