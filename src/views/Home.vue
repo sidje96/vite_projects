291992b60e92277c4a33ce65a161b76eb93d0bbf
sidje96 <sidneydamsma@gmail.com>
2026-03-04 15:18:37 +0100

    badges toegevoegd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,18 @@
         <v-row>
             <v-col cols="6">
                 <v-btn block :to="{ name: 'Scheduled' }" stacked>
-                    <v-icon class="mt-1" size="80">mdi-bookmark-outline</v-icon>
-                    <span class="textstyle text-sm-body-2 text-md-h6">Gepland</span>
+                    <v-badge location="top right" offset-x="5" offset-y="-15" content="+" color="custom-color">
+                        <v-icon class="mt-1" size="80">mdi-bookmark-outline</v-icon>
+                        <span class="textstyle text-sm-body-2 text-md-h6">Gepland</span>
+                    </v-badge>
                 </v-btn>
             </v-col>
             <v-col cols="6">
                 <v-btn block :to="{ name: 'Completed' }" stacked>
-                    <v-icon class="mt-1" size="80">mdi-check-circle</v-icon>
-                    <span class="textstyle text-sm-body-2 text-md-h6">Afgerond</span>
+                    <v-badge class="d-block" location="top right" offset-x="15" offset-y="-25" width="30" height="30" rounded="circle" :content="number" color="custom-color">
+                        <v-icon class="mt-1" size="80">mdi-check-circle</v-icon>
+                        <span class="textstyle text-sm-body-2 text-md-h6">Afgerond</span>
+                    </v-badge>  
                 </v-btn>
             </v-col>
         </v-row>
@@ -32,6 +36,15 @@
 </template>
 
 <script setup>
+import { useInspectionStore } from '@/stores/inspection';
+import { onMounted, ref } from 'vue';
+
+const store = useInspectionStore()
+onMounted(() => {
+    store.fetchInspections()
+})
+
+const number = store.sortedInspections.length
 
 </script>
 
