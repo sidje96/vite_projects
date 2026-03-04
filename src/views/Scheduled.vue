f7a3081bd9a345cd7c148e2c6d017347f6a85afa
sidje96 <sidneydamsma@gmail.com>
2026-03-04 15:19:35 +0100

    dezelfde button css bug als in de header opgelost. en gezorgd dat als je vanuit hier naar een nieuwe inspectie gaat, dat de form ook echt leeg is als je begint

--- src/views/Scheduled.vue
+++ src/views/Scheduled.vue
@@ -1,22 +1,36 @@
+<script setup>
+import router from '@/router';
+import { useInspectionStore } from '@/stores/inspection';
+
+const store = useInspectionStore()
+
+function newInspection() {
+    store.currentInspection = null
+    router.push({ name: 'Form'})
+}
+</script>
+
 <template>
     <v-sheet class="d-flex flex-sm-row flex-column justify-center align-center ga-10" min-height="100%" theme="custom-color">
         <v-btn
+            variant="elevated"
             size="x-large"
             class="text-h6 px-8 py-6"
             prepend-icon="mdi-plus"
             height="20vh"
-            :to="{ name: 'Form'}"
+            @click="newInspection"
             >
             Nieuwe inspectie
             </v-btn>
 
             <v-btn
+            variant="elevated"
             size="x-large"
             class="text-h6 px-8 py-6"
             prepend-icon="mdi-check"
             height="20vh"
             width="252.46"
-            :to="{ name: 'Completed'}"
+            @click="router.push({ name: 'Completed'})"
             >
             Afgerond
         </v-btn>
