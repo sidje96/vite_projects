979f54fb7a218582587eca6ccf30e06da8a2bfad
sidje96 <sidneydamsma@gmail.com>
2026-03-11 14:05:07 +0100

    loader toegevoegd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,53 +1,60 @@
 <template>
+    <v-overlay
+        :model-value="store.loadingInitial"
+        class="d-flex align-center justify-center"
+        persistent
+    >
+        <v-progress-circular indeterminate size="64" color="custom-color" />
+    </v-overlay>
     <v-container height="100%">
-        <v-row>
-            <v-col cols="6">
-                <v-btn block :to="{ name: 'Scheduled' }" stacked>
-                    <v-badge v-if="numberScheduled > 0" 
-                        class="d-block" 
-                        location="top right" 
-                        offset-x="-25"
-                        width="30" 
-                        height="30" 
-                        rounded="circle" 
-                        :content="numberScheduled" 
-                        color="custom-color"
-                        />                    
-                    <v-icon class="mt-1" size="80">mdi-bookmark-outline</v-icon>
-                    <span class="textstyle text-sm-body-2 text-md-h6">Gepland</span>
-                </v-btn>
-            </v-col>
-            <v-col cols="6">
-                <v-btn block :to="{ name: 'Completed' }" stacked>
-                    <v-badge v-if="numberCompleted > 0" 
-                        class="d-block" 
-                        location="top right" 
-                        offset-x="-25" 
-                        width="30" 
-                        height="30" 
-                        rounded="circle" 
-                        :content="numberCompleted" 
-                        color="custom-color"
-                        />
-                    <v-icon class="mt-1" size="80">mdi-check-circle</v-icon>
-                    <span class="textstyle text-sm-body-2 text-md-h6">Afgerond</span> 
-                </v-btn>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <v-btn block :to="{ name: 'KnowledgeBase' }" stacked>
-                    <v-icon class="mt-1" size="80">mdi-apps</v-icon>
-                    <span class="textstyle text-sm-body-2 text-md-h6">Kennisbase</span>
-                </v-btn>
-            </v-col>
-            <v-col>
-                <v-btn block :to="{ name: 'Settings' }" stacked>
-                    <v-icon class="mt-1" size="80">mdi-cog</v-icon>
-                    <span class="textstyle text-sm-body-2 text-md-h6">Instellingen</span>
-                </v-btn>
-            </v-col>
-        </v-row>
+            <v-row>
+                <v-col cols="6">
+                    <v-btn block :to="{ name: 'Scheduled' }" stacked>
+                        <v-badge v-if="store.numberScheduled > 0" 
+                            class="d-block" 
+                            location="top right" 
+                            offset-x="-25"
+                            width="30" 
+                            height="30" 
+                            rounded="circle" 
+                            :content="store.numberScheduled" 
+                            color="custom-color"
+                            />                    
+                        <v-icon class="mt-1" size="80">mdi-bookmark-outline</v-icon>
+                        <span class="textstyle text-sm-body-2 text-md-h6">Gepland</span>
+                    </v-btn>
+                </v-col>
+                <v-col cols="6">
+                    <v-btn block :to="{ name: 'Completed' }" stacked>
+                        <v-badge v-if="store.numberCompleted > 0" 
+                            class="d-block" 
+                            location="top right" 
+                            offset-x="-25" 
+                            width="30" 
+                            height="30" 
+                            rounded="circle" 
+                            :content="store.numberCompleted" 
+                            color="custom-color"
+                            />
+                        <v-icon class="mt-1" size="80">mdi-check-circle</v-icon>
+                        <span class="textstyle text-sm-body-2 text-md-h6">Afgerond</span> 
+                    </v-btn>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-btn block :to="{ name: 'KnowledgeBase' }" stacked>
+                        <v-icon class="mt-1" size="80">mdi-apps</v-icon>
+                        <span class="textstyle text-sm-body-2 text-md-h6">Kennisbase</span>
+                    </v-btn>
+                </v-col>
+                <v-col>
+                    <v-btn block :to="{ name: 'Settings' }" stacked>
+                        <v-icon class="mt-1" size="80">mdi-cog</v-icon>
+                        <span class="textstyle text-sm-body-2 text-md-h6">Instellingen</span>
+                    </v-btn>
+                </v-col>
+            </v-row>
     </v-container>
 </template>
 
@@ -56,18 +63,10 @@ import { useInspectionStore } from '@/stores/inspection';
 import { onMounted, computed } from 'vue';
 
 const store = useInspectionStore()
+
 onMounted(() => {
     store.fetchInspections()
 })
-
-const numberCompleted = computed(() =>
-  store.sortedInspections.filter(i => i.Status === "Completed").length
-)
-
-const numberScheduled = computed(() =>
-  store.sortedInspections.filter(i => i.Status === "Scheduled").length
-)
-
 </script>
 
 <style scoped>
