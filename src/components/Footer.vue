291de61380603816228f6ec9e425a2c875d80ed9
sidje96 <sidneydamsma@gmail.com>
2026-03-11 19:36:58 +0100

    Support Pagina in een dialog gezet zodat deze van onder het scherm kan vullen

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,53 @@
 <template>
-<v-bottom-navigation bg-color="custom-color" height="56" class="d-flex justify-center align-center">
-    <v-btn :to="{name: 'Support'}" stacked>
-        <v-icon>mdi-cellphone-information</v-icon>
-        <span class="text-caption pt-1">App support</span>
-    </v-btn>
-</v-bottom-navigation>
+  <v-bottom-navigation bg-color="custom-color" height="56" class="d-flex justify-center align-center">
+    <v-dialog v-model="dialog" transition="dialog-bottom-transition" fullscreen>
+      
+      <template v-slot:activator="{ props: activatorProps }">
+        <v-btn v-bind="activatorProps" stacked>
+          <v-icon>mdi-cellphone-information</v-icon>
+          <span class="text-caption pt-1">App support</span>
+        </v-btn>
+      </template>
+
+      <template v-slot:default="{ isActive }">
+        <v-card>
+          <v-toolbar color="custom-color">
+            <v-btn class="text-white mr-4" text="Sluiten" @click="isActive.value = false" />
+            <span class="text-h6">Klantenservice</span>
+            <v-spacer />
+            <v-btn text="Opslaan" @click="closeCustomersupport" />
+          </v-toolbar>
+
+          <SupportView />
+        </v-card>
+      </template>
+
+    </v-dialog>
+
+    <v-snackbar v-model="snackbar" color="warning" timeout="5000" close>
+      <span>Deze functionaliteit werkt nog niet voor dit prototype</span>
+      <template v-slot:actions>
+        <v-btn
+        size="small"
+          icon="mdi-close"
+          @click="snackbar = false"
+        >
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+  </v-bottom-navigation>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import SupportView from '@/views/Support.vue'
 
+const dialog = ref(false)
+const snackbar = ref(false)
+
+function closeCustomersupport() {
+  dialog.value = false
+  snackbar.value = true
+}
 </script>
