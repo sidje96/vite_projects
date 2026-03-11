b2b0cb74a541498d65a545662df271ec04d15a31
sidje96 <sidneydamsma@gmail.com>
2026-03-11 19:36:20 +0100

    transitie toegevoegd op het inlogscherm

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <router-view/>
+  <router-view v-slot="{ Component, route }">
+
+    <v-slide-x-transition v-if="['Login', 'TwoFactor'].includes(String(route.name))">
+      <component :is="Component" :key="route.fullPath" />
+    </v-slide-x-transition>
+
+    <component v-else :is="Component" :key="route.fullPath" />
+
+  </router-view>
 </template>
